0edd2bc259fd4c1046d6b9dc525f257b9a093c14
Jay <sardarjay4@gmail.com>
2021-07-01 23:38:51 +0530

    FIFTH commit

--- rest_api/rest_api/settings.py
+++ rest_api/rest_api/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0o==pb^g5_6d@(nxqxt6fl8m4sj$vs$+#ebry0@z^slttr^y6y'
+SECRET_KEY=os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'https://git.heroku.com/indbankdb.git','127.0.0.1:8000']
+ALLOWED_HOSTS = ['localhost','https://git.heroku.com/indbankdb.git','127.0.0.1:8000']
 DEBUG_PROPAGATE_EXCEPTIONS = False
 
 # Application definition
